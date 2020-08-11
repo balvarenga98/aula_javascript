066a17ab680ace6678b7b091bf145d124f2f685a
Breno Vinícius Alvarenga Sedano <breno_vinicius11@hotmail.com>
2020-08-11 17:37:10 -0300

    Introdução ao Javascript
    Final da aula | Digital Innovation One

---
+++ js/main.js
@@ -0,0 +1,55 @@
+function clicou() { document.getElementById("agradecimento").innerHTML = "Obrigado por clicar" };
+function redirecionar() { window.open("https://www.youtube.com/") };
+function trocar(elemento){elemento.innerHTML = "Obrigado por passar o mouse aqui "};
+function voltar(elemento){elemento.innerHTML = "Passe o mouse aqui"};
+function load(){alert ("Página carregada")};
+function funcaochange(elemento){alert (elemento.value)};
+//function clicou (){alert("Obrigado por clicar")};
+
+
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+alert(soma(5, 10)); /*
+
+
+//var d = new Date();
+
+
+//alert(d);
+
+
+/*  var count;
+for(count=1; count <=5; count++){alert(count)}*/
+
+
+/* var count = 0;
+while (count <= 5){alert(count);
+count++ ;} */
+
+
+/* var idade = prompt ("Qual sua idade?")
+if (idade >= 18) {alert ("maior de idade")
+} else {alert ("menor de idade")}; */
+
+
+/*var lista = ["Maçã", "Uva", "Laranja"];
+lista.push ("Banana");
+//lista.pop ();
+alert (lista[2]);*/
+
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join("  -  "));
+//var nome = "Breno Alvarenga";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem" + idade + " anos");
+//alert (idade + idade2);
+//console.log(nome);
+//console.log (n1 * n2);
+//console.log (frase.tolowerCase());
+//alert(frase.replace("Japão", "Brasil"));
+
